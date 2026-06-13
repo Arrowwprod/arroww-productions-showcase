0e0ad1479084f3fa4836cf88abe89b80bd41414c
Sahalben <sahalbenoffice@gmail.com>
2026-06-13 16:41:39 +0530

    Install and configure @netlify/vite-plugin-tanstack-start for SSR hosting support

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
+    netlify(),
     tanstackStart(),
     tailwindcss(),
     tsconfigPaths(),
